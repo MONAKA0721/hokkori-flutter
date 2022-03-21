5cb90b24477cd69bf2f13962f06787eeb41132cb
Yuya Sumie <monamonacan0926@gmail.com>
2022-03-21 21:14:21 +0900

    Setup GraphQLProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hokkori/home_page.dart';
 import 'package:hokkori/mail_page.dart';
 import 'package:hokkori/notification_page.dart';
 import 'package:hokkori/post_page.dart';
 import 'package:hokkori/search_page.dart';
 
-void main() {
+void main() async {
+  // We're using HiveStore for persistence,
+  // so we need to initialize Hive.
+  await initHiveForFlutter();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+const apiQueryURL = 'http://152.70.81.218:30818/query';
+final HttpLink httpLink = HttpLink(
+  apiQueryURL,
+);
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: httpLink,
+      // The default store is the InMemoryStore, which does NOT persist to disk
+      cache: GraphQLCache(store: HiveStore()),
+    ),
+  );
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hokkori',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const SafeArea(child: MyHomePage(title: 'ほっこり')),
-    );
+    return GraphQLProvider(
+        client: client,
+        child: MaterialApp(
+          title: 'Hokkori',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+          ),
+          home: const SafeArea(child: MyHomePage(title: 'ほっこり')),
+        ));
   }
 }
 
