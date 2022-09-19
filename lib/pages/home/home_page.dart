ad18f8f7d84f1d04b2a310012b3db939b1deea4e
Yuya Sumie <monamonacan0926@gmail.com>
2022-09-19 20:48:18 +0900

    もっと見るボタン

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:hokkori/pages/common/letter.dart';
 import 'package:hokkori/pages/common/praise.dart';
 import 'package:hokkori/pages/home/home_page.graphql.dart';
+import 'package:hokkori/pages/home/top_letters.dart';
 import 'package:hokkori/pages/home/work.dart';
 import 'package:hokkori/utils/colors.dart';
 import 'package:hokkori/utils/header.dart';
@@ -58,12 +58,8 @@ class HomePage extends StatelessWidget {
                   TopWorks(),
                   TopLetters(),
                   SizedBox(
-                    height: 30,
-                  ),
-                  Praises(),
-                  SizedBox(
                     height: 20,
-                  ),
+                  )
                 ],
               )))),
     ]);
@@ -171,175 +167,3 @@ class TopWorks extends HookWidget {
     ]);
   }
 }
-
-class Praises extends HookWidget {
-  const Praises({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final result = useQuery$TopPraises(Options$Query$TopPraises(
-            fetchPolicy: FetchPolicy.networkOnly,
-            variables: Variables$Query$TopPraises(first: 100)))
-        .result;
-
-    if (result.hasException) {
-      return Text(result.exception.toString());
-    }
-    if (result.isLoading) {
-      return const Center(
-        child: CircularProgressIndicator(
-          color: primaryColor,
-        ),
-      );
-    }
-    final praises = result.parsedData?.posts.edges ?? [];
-
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: const [
-          Icon(
-            Icons.favorite_border,
-            size: 30,
-            color: primaryColor,
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          Text(
-            "ほっこり",
-            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
-          ),
-        ],
-      ),
-      ...praises.map((praise) => Praise(praise: praise!.node!)).toList()
-    ]);
-  }
-}
-
-class TopLetters extends HookWidget {
-  const TopLetters({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final queryResult = useQuery$TopLetters(Options$Query$TopLetters(
-        fetchPolicy: FetchPolicy.networkOnly,
-        variables: Variables$Query$TopLetters(first: 3)));
-    final result = queryResult.result;
-    final fetchMore = queryResult.fetchMore;
-
-    if (result.hasException) {
-      return Text(result.exception.toString());
-    }
-    if (result.isLoading) {
-      return const Center(
-        child: CircularProgressIndicator(
-          color: primaryColor,
-        ),
-      );
-    }
-    final letters = result.parsedData?.posts.edges ?? [];
-    final String? fetchMoreCursor = result.parsedData?.posts.pageInfo.endCursor;
-    final opts = FetchMoreOptions$Query$TopLetters(
-      variables: Variables$Query$TopLetters(after: fetchMoreCursor, first: 3),
-      updateQuery: (previousResultData, fetchMoreResultData) {
-        // this is where you combine your previous data and response
-        // in this case, we want to display previous repos plus next repos
-        // so, we combine data in both into a single list of repos
-        final letters = [
-          ...previousResultData!['posts']['edges'] as List<dynamic>,
-          ...fetchMoreResultData!['posts']['edges'] as List<dynamic>
-        ];
-
-        // to avoid alot of work, lets just update the list of repos in returned
-        // data with new data, this also ensure we have the endCursor already set
-        // correctlty
-        fetchMoreResultData['posts']['edges'] = letters;
-
-        return fetchMoreResultData;
-      },
-    );
-
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: const [
-          Icon(
-            Icons.favorite_border,
-            size: 30,
-            color: primaryColor,
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          Text(
-            "レター",
-            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
-          ),
-        ],
-      ),
-      ...letters.map((letter) => Letter(letter: letter!.node!)).toList(),
-      const SizedBox(
-        height: 30,
-      ),
-      Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-        OutlinedButton(
-            style: OutlinedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-              backgroundColor: blueHomeColor,
-              side: const BorderSide(color: blueHomeColor, width: 2),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-            ),
-            onPressed: () {
-              fetchMore(opts);
-            },
-            child: Row(
-              children: const [
-                Text(
-                  "もっと見る",
-                  style: TextStyle(color: Colors.white),
-                ),
-                Icon(Icons.chevron_right, color: Colors.white)
-              ],
-            ))
-      ])
-    ]);
-  }
-}
-
-class FetchMoreButton extends StatefulWidget {
-  const FetchMoreButton({Key? key}) : super(key: key);
-
-  @override
-  State<FetchMoreButton> createState() => _FetchMoreButtonState();
-}
-
-class _FetchMoreButtonState extends State<FetchMoreButton> {
-  final 
-  @override
-  Widget build(BuildContext context) {
-    return OutlinedButton(
-        style: OutlinedButton.styleFrom(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-          backgroundColor: blueHomeColor,
-          side: const BorderSide(color: blueHomeColor, width: 2),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
-          ),
-        ),
-        onPressed: () {
-          fetchMore(opts);
-        },
-        child: Row(
-          children: const [
-            Text(
-              "もっと見る",
-              style: TextStyle(color: Colors.white),
-            ),
-            Icon(Icons.chevron_right, color: Colors.white)
-          ],
-        ));
-}
-
---
+++ lib/pages/home/top_letters.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:hokkori/pages/common/letter.dart';
+import 'package:hokkori/pages/home/home_page.graphql.dart';
+import 'package:hokkori/utils/colors.dart';
+
+class TopLetters extends HookWidget {
+  const TopLetters({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final result = useQuery$TopLetters(Options$Query$TopLetters(
+            fetchPolicy: FetchPolicy.networkOnly,
+            variables: Variables$Query$TopLetters(first: 3)))
+        .result;
+
+    if (result.hasException) {
+      return Text(result.exception.toString());
+    }
+    if (result.isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(
+          color: primaryColor,
+        ),
+      );
+    }
+    final letters = result.parsedData?.posts.edges ?? [];
+    final String? fetchMoreCursor = result.parsedData?.posts.pageInfo.endCursor;
+
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: const [
+          Icon(
+            Icons.favorite_border,
+            size: 30,
+            color: primaryColor,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            "レター",
+            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+          ),
+        ],
+      ),
+      ...letters.map((letter) => Letter(letter: letter!.node!)).toList(),
+      FetchMoreButton(cursor: fetchMoreCursor),
+    ]);
+  }
+}
+
+class FetchMoreButton extends HookWidget {
+  final String? cursor;
+  const FetchMoreButton({super.key, required this.cursor});
+
+  @override
+  Widget build(BuildContext context) {
+    final pushed = useState(false);
+    final result = useQuery$TopLetters(Options$Query$TopLetters(
+            variables: Variables$Query$TopLetters(first: 3, after: cursor)))
+        .result;
+
+    if (result.hasException) {
+      return Text(result.exception.toString());
+    }
+    if (result.isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(
+          color: primaryColor,
+        ),
+      );
+    }
+    final letters = result.parsedData?.posts.edges ?? [];
+    final fetchMoreCursor = result.parsedData?.posts.pageInfo.endCursor;
+    final hasNextPage = result.parsedData?.posts.pageInfo.hasNextPage;
+
+    return pushed.value
+        ? Column(children: [
+            ...letters.map((letter) => Letter(letter: letter!.node!)).toList(),
+            hasNextPage!
+                ? FetchMoreButton(
+                    cursor: fetchMoreCursor,
+                  )
+                : Container()
+          ])
+        : Column(children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              SizedBox(
+                  width: 280,
+                  child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 16, horizontal: 20),
+                        backgroundColor: blueHomeColor,
+                        side: const BorderSide(color: blueHomeColor, width: 2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () {
+                        pushed.value = true;
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          SizedBox(width: 24),
+                          Text(
+                            "もっと見る",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          Icon(Icons.expand_circle_down, color: Colors.white)
+                        ],
+                      )))
+            ])
+          ]);
+  }
+}
