a38b2b97f0854cb3ecb1b59d5d4f55d2f4adab59
Yuya Sumie <monamonacan0926@gmail.com>
2022-09-19 21:02:00 +0900

    ホーム画面

--- lib/pages/home/praises.dart
+++ lib/pages/home/praises.dart
@@ -26,6 +26,7 @@ class HomePraises extends HookWidget {
       );
     }
     final praises = result.parsedData?.posts.edges ?? [];
+    final String? fetchMoreCursor = result.parsedData?.posts.pageInfo.endCursor;
 
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       Row(
@@ -45,7 +46,80 @@ class HomePraises extends HookWidget {
           ),
         ],
       ),
-      ...praises.map((praise) => Praise(praise: praise!.node!)).toList()
+      ...praises.map((praise) => Praise(praise: praise!.node!)).toList(),
+      FetchMoreButton(
+        cursor: fetchMoreCursor,
+      )
     ]);
+  }
+}
+
+class FetchMoreButton extends HookWidget {
+  final String? cursor;
+  const FetchMoreButton({super.key, required this.cursor});
+
+  @override
+  Widget build(BuildContext context) {
+    final pushed = useState(false);
+    final result = useQuery$HomePraises(Options$Query$HomePraises(
+            variables: Variables$Query$HomePraises(first: 3, after: cursor)))
+        .result;
+
+    if (result.hasException) {
+      return Text(result.exception.toString());
+    }
+    if (result.isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(
+          color: primaryColor,
+        ),
+      );
+    }
+    final praises = result.parsedData?.posts.edges ?? [];
+    final fetchMoreCursor = result.parsedData?.posts.pageInfo.endCursor;
+    final hasNextPage = result.parsedData?.posts.pageInfo.hasNextPage;
+
+    return pushed.value
+        ? Column(children: [
+            ...praises.map((praise) => Praise(praise: praise!.node!)).toList(),
+            hasNextPage!
+                ? FetchMoreButton(
+                    cursor: fetchMoreCursor,
+                  )
+                : Container()
+          ])
+        : Column(children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              SizedBox(
+                  width: 280,
+                  child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 16, horizontal: 20),
+                        backgroundColor: blueHomeColor,
+                        side: const BorderSide(color: blueHomeColor, width: 2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () {
+                        pushed.value = true;
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          SizedBox(width: 24),
+                          Text(
+                            "もっと見る",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          Icon(Icons.expand_circle_down, color: Colors.white)
+                        ],
+                      )))
+            ])
+          ]);
   }
 }
