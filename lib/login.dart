e026f2587023b374ac4e3fc14223e37c5166e71d
Yuya Sumie <monamonacan0926@gmail.com>
2022-12-30 22:28:19 +0900

    ログイン画面

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hokkori/utils/colors.dart';
 
 class Login extends StatelessWidget {
   final Function loginAction;
@@ -8,20 +10,59 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        ElevatedButton(
-          onPressed: () {
-            loginAction();
-          },
-          child: const Text('Login'),
-        ),
-        Text(
-          loginError,
-          style: const TextStyle(fontSize: 16),
-        ),
-      ],
-    );
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.asset(
+              'assets/logo.svg',
+              color: primaryColor,
+              height: 100,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text("すべてのクリエイターにありがとうを",
+                style: TextStyle(
+                  decorationThickness: 0,
+                  color: primaryColor,
+                  fontSize: 16,
+                )),
+            const SizedBox(
+              height: 60,
+            ),
+            OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                  backgroundColor: blueButtonColor,
+                  side: const BorderSide(color: blueButtonColor, width: 2),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                onPressed: () {
+                  loginAction();
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(width: 24),
+                    const Text(
+                      "ログイン",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.white, width: 1)),
+                        child: const Icon(Icons.chevron_right,
+                            color: Colors.white))
+                  ],
+                )),
+          ],
+        ));
   }
 }
